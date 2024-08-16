c9d2f9dfb2615efeccd623c068634e8496759257
Saboor Hassan <156575127+HsnSaboor@users.noreply.github.com>
2024-08-16 22:16:34 +0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,6 +7,6 @@ def save_to_csv(data, filename):
     df.to_csv(filename, index=False)
 
 def classify_urls(urls):
-    google_maps_urls = [url for url in urls if "maps.google.com" in url]
+    google_maps_urls = [url for url in urls if "google.com/maps" in url]
     yellowpages_urls = [url for url in urls if "yellowpages.com" in url]
     return google_maps_urls, yellowpages_urls
