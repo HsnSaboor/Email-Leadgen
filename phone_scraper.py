198eeb416959f017d27758a854cb51141e65384d
Saboor Hassan <156575127+HsnSaboor@users.noreply.github.com>
2024-08-16 22:25:06 +0500

    Update phone_scraper.py

--- phone_scraper.py
+++ phone_scraper.py
@@ -1,4 +1,10 @@
 # phone_scraper.py
+import re
+import httpx
+from bs4 import BeautifulSoup
 
-def extract_phone_numbers(page):
-    return page.query_selector(".phone-number").text_content()
+def scrape_phone_numbers_from_page(url):
+    response = httpx.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    phones = re.findall(r'\+?\d[\d -]{8,}\d', soup.text)
+    return phones
