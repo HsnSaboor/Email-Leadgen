e91e460f3d12e1d5941f7765f79037916da4d6db
Saboor Hassan <156575127+HsnSaboor@users.noreply.github.com>
2024-08-16 22:33:41 +0500

    Update and rename utils.py to shop.py

---
+++ shop.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+@dataclass
+class Shop:
+    shop_name: str = None
+    shop_location: str = None
+    contact_number: str = None
+    website: str = None
+    email: str = None
+    average_review_count: str = None
+    average_review_points: str = None
