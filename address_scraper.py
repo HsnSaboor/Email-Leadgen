b4c261af51b61ea8dcd9f93289a776a066f11f31
Saboor Hassan <156575127+HsnSaboor@users.noreply.github.com>
2024-08-16 22:33:09 +0500

    Delete address_scraper.py

--- address_scraper.py
+++
@@ -1,9 +0,0 @@
-# address_scraper.py
-import httpx
-from bs4 import BeautifulSoup
-
-def scrape_address_from_page(url):
-    response = httpx.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    address = soup.find("address")
-    return address.text.strip() if address else "N/A"
